daf07848ac2c5b0d5355c57de98941922b9038ce
arch <archisha.chandel26@gmail.com>
2020-12-15 15:44:25 +0530

    adds files

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 # Number-Plate-Recognition-System
-Computer Vision Project(EC353).
 
 ### Built Using:
-* Python 2.7
+* Python 3.6
 * OpenCV 3.0.0
 * Tesseract 
-### Screenshot
-![Screenshot](/testData/SS1.png)
-
--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-ALLOWED_EXTENSIONS = set(['jpg','png'])
+ALLOWED_EXTENSIONS = set(['jpg','png', 'jpeg'])
 file_path = ''
 
 
